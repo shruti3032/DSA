b83c254c5c9364125ca7bde1b86e99e6af376985
Shruti Kamble <78202217+shruti3032@users.noreply.github.com>
2024-06-23 17:01:50 +0530

    Create Area_circle.java
    1st program from revision

---
+++ Mathematics/Area_circle.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    double r = sc.nextDouble();
+	    double Area = 3.14 * r * r;
+		System.out.println("Area of Circle: "+Area);
+	}
+}
